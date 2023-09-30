c5e346d0a2490ed18a898b1368c0f87d4cfff92d
Hoàng Gia Bảo <70064328+YT-Advanced@users.noreply.github.com>
2023-09-30 20:26:03 +0700

    Update IntegrationsPatch.kt

--- src/main/kotlin/app/revanced/patches/music/utils/integrations/patch/IntegrationsPatch.kt
+++ src/main/kotlin/app/revanced/patches/music/utils/integrations/patch/IntegrationsPatch.kt
@@ -1,6 +1,6 @@
 package app.revanced.patches.music.utils.integrations.patch
 
-import app.revanced.patcher.patch.annotations.RequiresIntegrations
+import app.revanced.patcher.patch.annotation.Patch
 import app.revanced.patches.music.utils.integrations.fingerprints.InitFingerprint
 import app.revanced.patches.shared.patch.integrations.AbstractIntegrationsPatch
 import app.revanced.util.integrations.Constants.MUSIC_INTEGRATIONS_PATH
