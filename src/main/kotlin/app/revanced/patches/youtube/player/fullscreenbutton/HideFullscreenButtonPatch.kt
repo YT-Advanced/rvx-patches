a8c61b2a28d8083a9b329e9770e214e9e44b7a89
Hoàng Gia Bảo <70064328+YT-Advanced@users.noreply.github.com>
2024-03-09 15:31:26 +0700

    Update HideFullscreenButtonPatch.kt

--- src/main/kotlin/app/revanced/patches/youtube/player/fullscreenbutton/HideFullscreenButtonPatch.kt
+++ src/main/kotlin/app/revanced/patches/youtube/player/fullscreenbutton/HideFullscreenButtonPatch.kt
@@ -10,7 +10,7 @@ import org.w3c.dom.Element
 
 @Patch(
     name = "Hide fullscreen button",
-    description = "Hides fullscreen button in player bottom UI container.",
+    description = "Force to hides fullscreen button in player bottom UI container.",
     dependencies = [SettingsPatch::class],
     compatiblePackages = [
         CompatiblePackage(
